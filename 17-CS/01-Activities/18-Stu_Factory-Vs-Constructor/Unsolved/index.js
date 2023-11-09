cd8523ff8ebe61f8afea457f0960da78e9e1c419
b0n3yard <namesake39@gmail.com>
2023-11-09 18:26:38 -0500

    pushing 19 - 21

--- 17-CS/01-Activities/18-Stu_Factory-Vs-Constructor/Unsolved/index.js
+++ 17-CS/01-Activities/18-Stu_Factory-Vs-Constructor/Unsolved/index.js
@@ -1,13 +1,32 @@
 // TODO: Refactor the class to a factory function.
-class Lesson {
-  constructor() {
-    this.title = 'Module 17 - Computer Science';
-    this.description = 'CS for JS';
+// class Lesson {
+//   constructor() {
+//     this.title = 'Module 17 - Computer Science';
+//     this.description = 'CS for JS';
+//   }
+//   information() {
+//     console.log(this.title, this.description);
+//   }
+// }
+
+const { title } = require("process")
+
+// const csForJS = new Lesson();
+// csForJS.information();
+
+function lesson(title, description){
+  let lessoninfo = {
+    title: title,
+    description: description
+
   }
-  information() {
-    console.log(this.title, this.description);
+  return {
+    information: function(){
+      console.log( lessoninfo.title, description)
+    }
   }
 }
 
-const csForJS = new Lesson();
-csForJS.information();
+const lessonplan = lesson('a title','a description')
+lessonplan.information()
+module.exports = lesson
